669ed86b239e94d8594f9b61cb056e68ca91ec9e
Fabiano César <fcfabiano.cesar@gmail.com>
2022-02-03 15:10:46 -0300

    checa se é o angulo é valido

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-01-primeiros-passos/exercicio5.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-01-primeiros-passos/exercicio5.js
@@ -1,8 +1,16 @@
 const a = 15;
 const b = 60;
-const c = 100;
+const c = -100;
 
 function checkNumber(a, b, c) {
+    if(
+    Math.sign(a) === -1 || 
+    Math.sign(b) === -1 ||
+    Math.sign(c) === -1
+    ) {
+        return "angulo inválido";
+    }
+
     if ((a + b + c) === 180) {
         return true;
     } else {
