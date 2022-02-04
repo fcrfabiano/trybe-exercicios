f83616376943a9d93bf8d44a3f3d5666e11f9519
Fabiano César <fcfabiano.cesar@gmail.com>
2022-02-04 16:13:24 -0300

    retorna o menor valor de uma lista

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
@@ -1,10 +1,18 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let oddNumbers = [];
 
-for (let i = 0; i < numbers.length; i += 1) {
-    if ((numbers[i] % 2) === 1) {
-        oddNumbers.push(numbers[i]);
+let lowerNumber = null;
+
+for (let i = 0; i < numbers.length; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+        if (numbers[j] < numbers[i]) {
+            if (lowerNumber === null) {
+                lowerNumber = numbers[j];
+            }
+            if (lowerNumber > numbers[j]) {
+                lowerNumber = numbers[j];
+            }
+        }     
     }
 }
 
-console.log(oddNumbers);
+console.log(lowerNumber);
