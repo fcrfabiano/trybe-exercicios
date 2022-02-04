7a455c8ba0709373aa964b79a115a1a91169b2d6
Fabiano César <fcfabiano.cesar@gmail.com>
2022-02-04 16:34:22 -0300

    Ordena uma array

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
@@ -1,7 +1,14 @@
-let numbers = [];
+let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let index = 0; index <= 25; index += 1) {
-    numbers.push(index / 2);
-}
 
-console.log(numbers);
+for (let index = 1; index < array.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (array[index] < array[secondIndex]) {
+        let position = array[index];
+        array[index] = array[secondIndex];
+        array[secondIndex] = position;
+      }
+    }
+  }
+
+  console.log(array);
