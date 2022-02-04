f4af17a45a4926538c6f7495d89c8fd5919aee34
Fabiano César <fcfabiano.cesar@gmail.com>
2022-02-04 15:54:54 -0300

    Encontra maior numero do array

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
@@ -1,9 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let total = 0;
+let biggerNum = 0;
 
-for (let number of numbers) {
-    total += number;
+for (let i = 0; i <= numbers.length; i+=1) {
+    for (let j = 0; j <= numbers.length; j++) {
+        if (numbers[j] > numbers[i]) {
+            if(biggerNum < numbers[j]) {
+                biggerNum = numbers[j];
+            }
+        }
+    }
 }
 
-console.log((total / numbers.length) > 20 ? "valor maior que 20" : "valor menor ou igual a 20");
+console.log(biggerNum);
