fd39a185a0318e00b497304e2f8292c786b92001
Fabiano César <fcfabiano.cesar@gmail.com>
2022-02-04 16:14:51 -0300

    Cria array com numeros de 1 a 25

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
@@ -1,18 +1,7 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [];
 
-let lowerNumber = null;
-
-for (let i = 0; i < numbers.length; i++) {
-    for (let j = 0; j < numbers.length; j++) {
-        if (numbers[j] < numbers[i]) {
-            if (lowerNumber === null) {
-                lowerNumber = numbers[j];
-            }
-            if (lowerNumber > numbers[j]) {
-                lowerNumber = numbers[j];
-            }
-        }     
-    }
+for (let index = 0; index <= 25; index += 1) {
+    numbers.push(index);
 }
 
-console.log(lowerNumber);
+console.log(numbers);
