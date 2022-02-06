1fa7672340c1a58e1ee66107e0af14548a929c88
Fabiano César <fcfabiano.cesar@gmail.com>
2022-02-06 17:25:00 -0300

    Imprime triangulos invertido

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio2.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio2.js
@@ -1,7 +1,6 @@
 function inverterWord(word) {
     const wordArray = word.split("");
     const invertedWord = [];
-    const invertedWordString = "";
 
     for (let i = wordArray.length; i >= 0; i -= 1) {
         invertedWord.push(wordArray[i]);
