f42572b3c40b70ecf86fd0ce58c4e37d26d71a5c
Fabiano César <fcfabiano.cesar@gmail.com>
2022-02-10 15:47:54 -0300

    Acesse o primeiroFilho a partir de pai .

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -2,3 +2,6 @@ const ondeVoceEsta = document.getElementById("elementoOndeVoceEsta");
 ondeVoceEsta.parentElement.style.backgroundColor = "red";
 
 ondeVoceEsta.firstElementChild.innerText = "Primeiro Filho";
+
+const pai = document.getElementById("pai");
+const primeiroFilho = pai.firstElementChild;
