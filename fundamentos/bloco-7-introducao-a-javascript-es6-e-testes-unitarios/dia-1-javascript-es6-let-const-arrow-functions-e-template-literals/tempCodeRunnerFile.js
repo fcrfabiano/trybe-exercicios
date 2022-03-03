0b08ae18c984ac94fd4012e97f019abff04db82c
Fabiano César <fcfabiano.cesar@gmail.com>
2022-03-03 18:59:23 -0300

    Desafio 4 e 5

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortNumber = () => oddsAndEvens.sort((a, b) => a - b);
+
+console.log(`Os números ${sortNumber()} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
