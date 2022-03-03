a823dd71a223e6540fb8f7718de9b0c7b47edfac
Fabiano César <fcfabiano.cesar@gmail.com>
2022-03-03 19:58:01 -0300

    fim do exercicio

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-
-const longestWord = (word) => console.log(word.split(' ').sort((a, b) => a.length - b.length).pop());
-
-longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'); // retorna 'aconteceu'
