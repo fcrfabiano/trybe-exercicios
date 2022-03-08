14cb3a2544491f2c5586408c8883191bd2f345fe
Fabiano César <fcfabiano.cesar@gmail.com>
2022-03-08 17:06:59 -0300

    Separa a funçao do teste em arquivos diferentes

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/__tests__/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/__tests__/sum.test.js
@@ -1,10 +1,4 @@
-function sum(a, b) {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-      throw new Error('parameters must be numbers');
-    }
-  
-    return a + b;
-}
+const sum = require('../sum.js');
 
 describe("Testa a função sum que retorna a soma de dois números", () => {
     test("Teste se o retorno de sum(4, 5) é 9", () => {
