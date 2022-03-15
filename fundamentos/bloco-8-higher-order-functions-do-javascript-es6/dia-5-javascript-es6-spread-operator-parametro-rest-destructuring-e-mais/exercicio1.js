b46f5fe35de7771976c6328ba6bf5ce6317b67d1
Fabiano César <fcfabiano.cesar@gmail.com>
2022-03-15 20:28:27 -0300

    Exercicio 4

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio1.js
@@ -1,8 +1,14 @@
 const rectangleArea = (width, height) => width * height;
 
-const rectangles = [[1, 2], [3, 5], [6, 9]];
+//const rectangles = [[1, 2], [3, 5], [6, 9]];
+const rectangle1 = [1, 2];
+const rectangle2 = [3, 5];
+const rectangle3 = [6, 9];
+const rectangles = [rectangle1, rectangle2, rectangle3];
 
 rectangles.forEach((rectangle) => {
-    const [a, b] = rectangle;
-  console.log(rectangleArea(a, b));
+  rectangleArea(...rectangle)
+  console.log(rectangle[0] * rectangle[1]);
+  //   const [a, b] = rectangle;
+  // console.log(rectangleArea(a, b));
 });
