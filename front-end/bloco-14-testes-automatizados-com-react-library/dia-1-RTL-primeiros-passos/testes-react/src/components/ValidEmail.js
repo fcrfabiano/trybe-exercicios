dc8af52951e8bab311eb1c68d56e12d7ba3e147e
Fabiano César <fcfabiano.cesar@gmail.com>
2022-05-08 16:25:17 -0300

    feat: implements tests to valid email

---
+++ front-end/bloco-14-testes-automatizados-com-react-library/dia-1-RTL-primeiros-passos/testes-react/src/components/ValidEmail.js
@@ -0,0 +1,26 @@
+// ValidEmail.js
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './styles.css';
+
+const verifyEmail = (email) => {
+  const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+  return emailRegex.test(email);
+};
+
+const ValidEmail = (props) => {
+  const { email } = props;
+  return (
+    <div>
+      <h2 data-testid="id-email-user">{`Valor: ${email}`}</h2>
+      <h3 data-testid="id-is-email-valid" className={verifyEmail(email) ? 'green-text' : 'red-text'}>{(verifyEmail(email) ? 'Email Válido' : 'Email Inválido')}</h3>
+    </div>
+  );
+};
+
+ValidEmail.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default ValidEmail;
